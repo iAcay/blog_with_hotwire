7a9d8e5109b89890278be5725191187a2f37e2a6
iAcay <96024046+iAcay@users.noreply.github.com>
2022-11-02 19:07:28 +0100

    feat: introduce comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_28_165056) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_180022) do
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_28_165056) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "user_id", null: false
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_28_165056) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
