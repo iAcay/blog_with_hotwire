c717280dcc264a5833cc4ce19799ab81c30fccfb
iAcay <96024046+iAcay@users.noreply.github.com>
2022-11-10 22:34:31 +0100

    refactor: remove unnecessary comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,20 @@
 class ArticlesController < HomeController
   before_action :set_article, only: %i[ show edit update destroy ]
 
-  # GET /articles or /articles.json
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1 or /articles/1.json
   def show
   end
 
-  # GET /articles/new
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
     @article.user = current_user
@@ -35,7 +30,6 @@ class ArticlesController < HomeController
     end
   end
 
-  # PATCH/PUT /articles/1 or /articles/1.json
   def update
     respond_to do |format|
       if @article.update(article_params)
@@ -48,7 +42,6 @@ class ArticlesController < HomeController
     end
   end
 
-  # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
 
@@ -59,12 +52,10 @@ class ArticlesController < HomeController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def article_params
       params.require(:article).permit(:title, :content, :user_id)
     end
